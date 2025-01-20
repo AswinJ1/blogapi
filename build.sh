a10ac67c3bf9a2313b2d5d50b0104363333a9b30
AswinJ1 <aswinjhome@gmail.com>
2025-01-20 13:21:44 +0530

    Initial commit

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit  # Exit immediately if a command exits with a non-zero status
 # Install dependencies
 pip install -r requirements.txt  # Corrected the command to use '-r' instead of '-'
 
-# Collect static files without prompting for input
-python manage.py collectstatic --no-input
+
 
 # Run database migrations
 python manage.py migrate
